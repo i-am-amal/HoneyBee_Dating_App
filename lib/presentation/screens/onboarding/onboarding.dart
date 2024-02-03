262d32107345b93bc003eb09bf19e0d1362c5add
i-am-amal <amald1011@gmail.com>
2024-02-03 18:35:58 +0530

    onboarding screen completly finsihed

--- lib/presentation/screens/onboarding/onboarding.dart
+++ lib/presentation/screens/onboarding/onboarding.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:honeybee/presentation/screens/sign_in/sign_in_page/sign_in_page.dart';
 import 'package:honeybee/presentation/widgets/button_widgets/main_custom_button.dart';
@@ -14,89 +12,82 @@ class Onboarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // double width = MediaQuery.of(context).size.width;
-    // double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 70,
-            ),
-            const ImageWidget(
-                imagepath:
-                    'https://images.pexels.com/photos/935789/pexels-photo-935789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-            const SizedBox(
-              height: 50,
-            ),
-            const HeadingText(
-                text: "Algorithm",
-                fontWeight: FontWeight.w900,
-                fontsize: 25,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: height * 0.1,
+              ),
+              const ImageWidget(
+                  imagepath:
+                      'https://images.pexels.com/photos/935789/pexels-photo-935789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              SizedBox(
+                height: height * 0.1,
+              ),
+              const HeadingText(
+                  text: "Welcome to HoneyBee",
+                  fontWeight: FontWeight.w900,
+                  fontsize: 25,
+                  fontFamily: CustomFont.headTextFont,
+                  textColor: CustomColors.kHeadTextOnboardingColor),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              CustomText(
+                height: height * 0.08,
+                width: width * 0.8,
+                text:
+                    "Spark meaningful connections with your words—beyond just swipes.🍯💖",
+                textColor: CustomColors.kBlackTextColor,
+                fontWeight: FontWeight.normal,
+                fontFamily: CustomFont.textFont,
+                fontsize: 14,
+              ),
+              SizedBox(
+                height: height * 0.06,
+              ),
+              MainCustomButton(
+                width: width * 0.25,
+                height: height * 0.017,
+                customtext: "Let's Buzz...🐝",
+                txtcolor: CustomColors.kWhiteTextColor,
+                fontWeight: FontWeight.bold,
+                fontsize: 15,
+                letterspacing: 1.5,
                 fontFamily: CustomFont.headTextFont,
-                textColor: CustomColors.kHeadTextOnboardingColor),
-            const SizedBox(
-              height: 20,
-            ),
-            const CustomText(
-              height: 50,
-              width: 250,
-              text:
-                  "Users going through a vetting process to ensure you never match with bots.",
-              textColor: CustomColors.kBlackTextColor,
-              fontWeight: FontWeight.normal,
-              fontFamily: CustomFont.textFont,
-              fontsize: 14,
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            MainCustomButton(
-              width: 114,
-              height: 14,
-              customtext: "Continue",
-              txtcolor: CustomColors.kWhiteTextColor,
-              fontWeight: FontWeight.bold,
-              fontsize: 15,
-              letterspacing: 1.5,
-              fontFamily: CustomFont.headTextFont,
-              onpressed: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(
-                //     builder: (context) => const SignInPage(),
-                //   ),
-                // );
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) =>
-                        const SignInPage(),
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                      const begin = Offset(1.0, 0.0);
-                      const end = Offset.zero;
-                      const curve = Curves.easeInOutQuart;
-                      var tween = Tween(begin: begin, end: end)
-                          .chain(CurveTween(curve: curve));
-                      var offsetAnimation = animation.drive(tween);
-                      return SlideTransition(
-                          position: offsetAnimation, child: child);
-                    },
-                  ),
-                );
-
-                log('---------------to signin page ..pushing');
-              },
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            const CustomText(
-              text: 'Already have an account? Sign-In',
-              fontWeight: FontWeight.w500,
-              textColor: Colors.black54,
-            ),
-          ],
+                onpressed: () {
+                  //--------page transition using page route builder
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          const SignInPage(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOutQuart;
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+                        return SlideTransition(
+                            position: offsetAnimation, child: child);
+                      },
+                    ),
+                  );
+                  //-------page route builder ends used for transition
+                },
+              ),
+              SizedBox(
+                height: height * 0.01,
+              ),
+            ],
+          ),
         ),
       ),
     );
